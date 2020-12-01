320638ae4f87cd62075f5e080220cee5dd1becce
Patrik Jansson <dkpj90@gmail.com>
2020-12-01 21:51:30 +0100

    Changed the controller template to use specific controller impl. created a symlink for this to work. This is just a minor test. More changes is needed

--- run-dev.cmd
+++ run-dev.cmd
@@ -15,6 +15,7 @@ REM install application
 docker exec pjython python3 /app/src-gen/setup.py install 
 
 REM create sym link to impl folder
+docker exec pjython ln -s /app/fs/be/src/controller /app/src-gen/openapi_server/controller
 
 REM Start flask
 docker exec pjython python3 -m openapi_server
